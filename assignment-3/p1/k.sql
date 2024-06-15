a381153eaf38a466526553c9d11a98d289c69870
Eitan Barylko <eitan@barylko.com>
2024-06-15 09:59:05 -0700

    Added all the violating queries

---
+++ assignment-3/p1/k.sql
@@ -0,0 +1,1 @@
+UPDATE Broker set pid = 1001001000 where pid = 2002002000;
--- assignment-3/p1/l.sql
+++ assignment-3/p1/l.sql
@@ -1,1 +1,2 @@
 -- Write an INSERT statement on Trade that fails for violating (e).
+INSERT into Trade values (100, 4, 'sell', '2024-01-31 13:00:00', 'AAPL', 11, 143.68);
