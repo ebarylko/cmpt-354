40edc8e712186a7781f2d221e763b1547df6dcee
Eitan Barylko <eitan@barylko.com>
2024-06-28 17:44:07 -0700

    Changed return value of holdings so a float is always returned

--- assignment-3/p2/app.py
+++ assignment-3/p2/app.py
@@ -1,6 +1,6 @@
 from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import text, select
+from sqlalchemy import text
 import time, datetime
 
 
@@ -41,6 +41,8 @@ def getOwner():
     response = [{"pid": int(pid)} for pid in pids]
     return jsonify(response or [{"pid": -1}])
 
+aid_and_sym_validation_query = text("select exists(select aid from account where aid=:aid) and exists(select sym from stock where sym=:sym);")
+
 @app.route('/getHoldings')
 def getHoldings():
     """
@@ -53,31 +55,31 @@ def getHoldings():
         """
     aid = int(request.args.get('aid', -1))
     sym = request.args.get('sym', '')
-    validation_query = text("select exists(select aid from account where aid=:aid) and exists(select sym from stock where sym=:sym);")
-    aid_and_sym_exists = db.session.execute(validation_query, {"aid": aid, "sym": sym}).scalar()
+    aid_and_sym_exists = db.session.execute(aid_and_sym_validation_query, {"aid": aid, "sym": sym}).scalar()
     shares = -1
     if aid_and_sym_exists:
-        query = text("select coalesce(sum(case when type = 'buy' then shares else shares * -1 end), 0) as shares from trade T where T.sym = :sym and T.aid = :aid")
-        shares = db.session.execute(query, {"sym": sym, "aid": aid}).scalar()
-    return jsonify({'shares': shares})
+        total_holdings_query = text("select coalesce(sum(case when type = 'buy' then shares else shares * -1 end), 0) as shares "
+                                    "from trade T where T.sym = :sym and T.aid = :aid")
+        shares = db.session.execute(total_holdings_query, {"sym": sym, "aid": aid}).scalar()
+    return jsonify({'shares': float(shares)})
 
 def currentTime():
     ts = time.time()
     return datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S')
-    
+
 @app.route('/trade')
 def trade():
     """
-        This HTTP method takes the information of a trade as input: aid, sym, type, shares, price 
-        You need to retrieve the current maximum seq numer (max_seq) for aid and 
+        This HTTP method takes the information of a trade as input: aid, sym, type, shares, price
+        You need to retrieve the current maximum seq numer (max_seq) for aid and
         then insert with max_seq+1 and the current timestamp.
         It returns {'res' : 'fail'} if there is an oversell;
         Otherwise, it returns {'res': the current seq} and also updates the database accordingly.
-        You should implement the check of the violation in the Python function 
+        You should implement the check of the violation in the Python function
         and DO NOT use the view you implemented in P1.
         You will need to finish a multi statement transaction in this function.
-        
-        Ideally, you need to send a HTTP POST request for such editing requests, 
+
+        Ideally, you need to send a HTTP POST request for such editing requests,
         but we just choose GET for easier test
     """
     aid = int(request.args.get('aid', -1))
@@ -85,12 +87,14 @@ def trade():
     type = request.args.get('type', '')
     shares = float(request.args.get('shares', -1))
     price = float(request.args.get('price', -1))
-    validation_query = text("select exists(select aid from account where aid=:aid) and exists(select sym from stock where sym=:sym);")
+
     shares_query = text("select coalesce(sum(case when type = 'buy' then shares else shares * -1 end), -1) from trade T where T.aid=:aid and T.sym=:sym")
-    aid_and_sym_exists = db.session.execute(validation_query, {"aid": aid, "sym": sym}).scalar()
+    aid_and_sym_exists = db.session.execute(aid_and_sym_validation_query, {"aid": aid, "sym": sym}).scalar()
+
     total_shares = db.session.execute(shares_query, {"aid": aid, "sym": sym}).scalar()
     is_not_oversell = type == 'buy' or total_shares >= shares
     res = "fail"
+
     if aid_and_sym_exists and is_not_oversell:
         latest_seq_id = db.session.execute(text("select coalesce(max(seq), 0) from trade where aid=:aid"),
                                            {"aid": aid}).scalar() + 1
@@ -100,17 +104,17 @@ def trade():
 
         add_trade_stmt = text("Insert into trade values (:aid, :seq, :type, :timestamp, :sym, :shares, :price)")
         db.session.execute(add_trade_stmt, {"aid": aid,
-                                             "seq": latest_seq_id,
-                                             "type": type,
-                                             "timestamp": latest_trade_time,
-                                             "sym": sym,
-                                             "shares": shares,
-                                             "price": price})
+                                            "seq": latest_seq_id,
+                                            "type": type,
+                                            "timestamp": latest_trade_time,
+                                            "sym": sym,
+                                            "shares": shares,
+                                            "price": price})
         db.session.commit()
 
         res = latest_seq_id
 
-    return jsonify({"id": res, "aid": aid})
+    return jsonify({"res": res})
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
--- assignment-3/p2/test_app.py
+++ assignment-3/p2/test_app.py
@@ -4,8 +4,7 @@ import pytest
 
 
 @pytest.mark.parametrize("path, expected", [("/getOwner?aid=800", "800"),
-                                            ("/getOwner?aid=150", '{"pid":1001001000},'
-                                                                  '{"pid":5005005000}'),
+                                            ("/getOwner?aid=150", '{"pid":1001001000},{"pid":5005005000}'),
                                             ("/getOwner?aid=1000", "-1")])
 def test_getOwner(path, expected):
     client = app.test_client()
@@ -15,6 +14,7 @@ def test_getOwner(path, expected):
 
 @pytest.mark.parametrize("path, expected", [("/getHoldings?aid=150&sym=AAPL", "4.00"),
                                             ("/getHoldings?aid=1000&sym=AAPL", "-1"),
+                                            ("/getHoldings?aid=150&sym=se", "-1"),
                                             ("/getHoldings?aid=800&sym=AAPL", "0")])
 def test_getHoldings(path, expected):
     client = app.test_client()
@@ -23,7 +23,9 @@ def test_getHoldings(path, expected):
 
 
 @pytest.mark.parametrize("path, expected", [("/trade?aid=300&sym=GOOGL&type=buy&shares=100.00&price=99.99", "2"),
-                                            ("/trade?aid=138&sym=AAPL&type=sell&shares=101.00&price=200.20", "fail")])
+                                            ("/trade?aid=138&sym=AAPL&type=sell&shares=101.00&price=200.20", "fail"),
+                                            ("/trade?aid=188&sym=AAPL&type=buy&shares=101.00&price=200.20", "fail"),
+                                            ("/trade?aid=138&sym=A&type=buy&shares=101.00&price=200.20", "fail")])
 def test_trade_response(path, expected):
     client = app.test_client()
     response = client.get(path).text
@@ -31,22 +33,22 @@ def test_trade_response(path, expected):
 
 
 @pytest.mark.parametrize('path, expected_values, pred', [("/trade?aid=300&sym=GOOGL&type=buy&shares=100.00&price=99.99", (300, 'GOOGL', 'buy', 100.00, 99.99, 2), "exists"),
-                                                         # ("/trade?aid=138&sym=APPL&type=sell&shares=101.00&price=200.20", (138, 'APPL', 'sell', 101.00, 200.20, 5), "not exists")
+                                                         ("/trade?aid=138&sym=APPL&type=sell&shares=101.00&price=200.20", (138, 'APPL', 'sell', 101.00, 200.20, 5), "not exists"),
+                                                         ("/trade?aid=800&sym=GOOGL&type=sell&shares=4.00&price=200.20", (800, 'GOOGL', 'sell', 4, 200.20, 3), "not exists")
                                                          ])
 def test_table_state_after_trade(path, expected_values, pred):
     client = app.test_client()
     expected_aid, expected_sym, expected_type, expected_shares, expected_price, expected_trade_seq = expected_values
-    # print(expected_values)
     new_trade_query = text("select {pred}(select * from trade where aid=:aid and sym=:sym and type=:type and shares=:shares and seq=:trade_id and price=:price)".
                            format(pred=pred))
     client.get(path)
     with app.app_context():
         res = db.session.execute(new_trade_query, {"aid": expected_aid,
-                                                    "sym": expected_sym,
-                                                    "type": expected_type,
-                                                    "shares": expected_shares,
-                                                    "trade_id": expected_trade_seq,
-                                                    "price": expected_price
-                                                    }).all()
+                                                   "sym": expected_sym,
+                                                   "type": expected_type,
+                                                   "shares": expected_shares,
+                                                   "trade_id": expected_trade_seq,
+                                                   "price": expected_price
+                                                   }).all()
         assert res
         db.session.rollback()
